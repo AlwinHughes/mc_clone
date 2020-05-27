41b2e4a4e835b840b26f441a1ad0be257d44357d
Alwin Hughes <alwin.hughes@btopenworld.com>
2020-05-28 00:11:57 +0100

    written abstract class for chunk host that only loads chunks in a certain range

--- Assets/Scripts/Chunk/FlatChunkHost.cs
+++ Assets/Scripts/Chunk/FlatChunkHost.cs
@@ -8,18 +8,6 @@ public class FlatChunkHost : IChunkHost<FlatChunk, NoiseGen1Geom>  {
   [SerializeField]
   public GeomNoiseSettings geom_noise_set;
 
-
-  void Start() {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-
-
   override protected NoiseGen1Geom getNoiseGen(int i, int j) {
     return new NoiseGen1Geom(
         new NoiseSetting(noise_set, new Vector3(i * noise_set.scale_x, j * noise_set.scale_y,0)),
---
+++ Assets/Scripts/Settings/LocalisedChunkSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class LocalisedChunkSettings : ScriptableObject {
+
+  [Range(1f, 100f)]
+  public float range = 5f;
+}
