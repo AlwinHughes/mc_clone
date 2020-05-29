d9bde97b4549a87f7e6741b6ffcd32a7a3329c7f
Alwin Hughes <alwin.hughes@btopenworld.com>
2020-05-29 11:04:22 +0100

    BlockyV3Chunk can now display textures using material and uv cords

--- Assets/Scripts/Chunk/BlockyChunkV3.cs
+++ Assets/Scripts/Chunk/BlockyChunkV3.cs
@@ -17,6 +17,8 @@ public class BlockyChunkV3 : IChunkCollider {
   }
   */
 
+  private List<Vector2> uvs;
+
 
   override protected void updateMesh() {
     Debug.Log("bcv3 updating");
@@ -25,6 +27,7 @@ public class BlockyChunkV3 : IChunkCollider {
     //triangles = new int[(chunk_set.res_x -1) * (chunk_set.res_y -1) *6];
 
     verts2 = new List<Vector3>(4 * (chunk_set.res_x ) * (chunk_set.res_y ));
+    uvs = new List<Vector2>(4 * (chunk_set.res_x ) * (chunk_set.res_y ));
     triangles2 = new List<int>((chunk_set.res_x -1) * (chunk_set.res_y -1) *6);
 
     int tri_index = 0;
@@ -44,26 +47,31 @@ public class BlockyChunkV3 : IChunkCollider {
     float y2;
     float y3;
 
+    Vector2 top_left = new Vector2(0f,1f);
+    Vector2 top_right = new Vector2(1f,1f);
+    Vector2 bot_left = new Vector2(0,0);
+    Vector2 bot_right = new Vector2(1,0);
+
     for(int i = 0; i < chunk_set.res_x; i++) {
       for(int j = 0; j < chunk_set.res_y; j++) {
-
-
-        /*
-           if(i == chunk_set.res_x -1 || j != chunk_set.res_y -1) {
-           continue;
-           }
-           */
-
 
         if(i != chunk_set.res_x -1 && j != chunk_set.res_y -1) {
 
           y = noise[i + chunk_set.res_x * j];
-         
           
           verts2.Add(new Vector3(i * inv_x_res, y, j * inv_y_res));
+          uvs.Add(bot_left);
+
           verts2.Add(new Vector3( (i+1) * inv_x_res, y, j * inv_y_res));
+          uvs.Add(bot_right);
+
           verts2.Add(new Vector3( i * inv_x_res, y, (j +1) * inv_y_res));
+          uvs.Add(top_left);
+
           verts2.Add(new Vector3( (i+1) * inv_x_res, y, (j+1) * inv_y_res));
+          uvs.Add(top_right);
+
+
 
           triangles2.Add(vert_index);
           triangles2.Add(vert_index + 3);
@@ -81,9 +89,25 @@ public class BlockyChunkV3 : IChunkCollider {
           if(y2 != y) {
 
             verts2.Add(new Vector3( (i + 1) * inv_x_res, y2, j * inv_y_res));
+
             verts2.Add(new Vector3( (i + 1) * inv_x_res, y2, (j + 1) * inv_y_res));
+
             verts2.Add(new Vector3( (i + 1) * inv_x_res, y, j * inv_y_res));
+            
+
             verts2.Add(new Vector3( (i + 1) * inv_x_res, y, (j + 1) * inv_y_res));
+
+            if(y2 > y) {
+              uvs.Add(top_right);
+              uvs.Add(top_left);
+              uvs.Add(bot_right);
+              uvs.Add(bot_left);
+            } else {
+              uvs.Add(bot_left);
+              uvs.Add(bot_right);
+              uvs.Add(top_left);
+              uvs.Add(top_right);
+            }
 
             triangles2.Add(vert_index);
             triangles2.Add(vert_index + 3);
@@ -103,9 +127,29 @@ public class BlockyChunkV3 : IChunkCollider {
           if(y3 != y) {
 
             verts2.Add(new Vector3(i * inv_x_res, y3, (j + 1) * inv_y_res));
+            
+
             verts2.Add(new Vector3((i + 1) * inv_x_res, y3, (j + 1) * inv_y_res));
+            
+
             verts2.Add(new Vector3(i * inv_x_res, y, (j + 1) * inv_y_res));
+            
+
             verts2.Add(new Vector3((i + 1) * inv_x_res, y, (j + 1) * inv_y_res));
+
+
+            if(y3 > y) {
+              uvs.Add(top_left);
+              uvs.Add(top_right);
+              uvs.Add(bot_left);
+              uvs.Add(bot_right);
+            } else {
+              uvs.Add(bot_right);
+              uvs.Add(bot_left);
+              uvs.Add(top_right);
+              uvs.Add(top_left);
+            }
+            
 
             triangles2.Add(vert_index);
             triangles2.Add(vert_index + 1);
@@ -128,6 +172,7 @@ public class BlockyChunkV3 : IChunkCollider {
     mesh_filter.sharedMesh.Clear();
     mesh_filter.sharedMesh.vertices = verts2.ToArray();
     mesh_filter.sharedMesh.triangles = triangles2.ToArray();
+    mesh_filter.sharedMesh.uv= uvs.ToArray();
     mesh_filter.sharedMesh.RecalculateNormals();
 
     mesh_collider.sharedMesh = mesh_filter.sharedMesh; }
